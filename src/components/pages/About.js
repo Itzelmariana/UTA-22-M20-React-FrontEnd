1227c422e70f02bab58c415b52d0a18577ac2c2e
Itzel <104706728+Itzelmariana@users.noreply.github.com>
2022-10-06 17:31:54 -0500

    deleted unused files

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,9 +14,13 @@ export default function About() {
           />
         </div>
         <div className='col-md-9 col-sm-12'>
-          I am currently seeking new opportunities in front-end development. I
-          am open to work remotely and have flexible hours. I am highly skilled
-          in JavaScript and React.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
     </div>
